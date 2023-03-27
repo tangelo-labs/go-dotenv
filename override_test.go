e6c96ea5a6b1091345122bb21a63dbecf212afd8
qfornaguera <qfornaguera@gmail.com>
2023-03-27 08:57:29 +0200

    Proving Override feature is not thread-safe

--- override_test.go
+++ override_test.go
@@ -1,9 +1,14 @@
 package dotenv_test
 
 import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tangelo-labs/go-dotenv"
 )
@@ -88,10 +93,48 @@ func TestWithOverride(t *testing.T) {
 			})
 		})
 	})
+
+	t.Run("GIVEN an environment variable", func(t *testing.T) {
+		require.NoError(t, os.Setenv("GIT_GUT", "lol"))
+
+		t.Run("WHEN multiple goroutines are overriding THEN each goroutine should be its value", func(t *testing.T) {
+			muxAssert := sync.Mutex{}
+			wg := sync.WaitGroup{}
+
+			for i := 0; i < 100; i++ {
+
+			}
+
+			for i := 0; i < 100; i++ {
+				wg.Add(1)
+				go func(idx int) {
+					val := fmt.Sprintf("%d", rand.New(rand.NewSource(int64(idx))).Int63())
+					fmt.Printf("val %d: %s\n", idx, val)
+
+					dotenv.WithOverride(func() {
+						defer wg.Done()
+						var env loneVarTestEnv
+						err := dotenv.LoadAndParse(&env)
+
+						muxAssert.Lock()
+						assert.NoErrorf(t, err, "error on idx %d", idx)
+						assert.EqualValuesf(t, val, env.YRURunning, "assert fail on idx %d", idx)
+						muxAssert.Unlock()
+					}, "GIT_GUT", val)
+				}(i)
+			}
+
+			wg.Wait()
+		})
+	})
 }
 
 type testEnv struct {
 	Foo                                      string   `env:"FOO" default:"bar"`
 	TheMeaningOfLifeTheUniverseAndEverything int      `env:"DUMMY" default:"42"`
 	FakeList                                 []string `env:"FAKE_LIST" default:"foo,bar" delimiter:","`
+}
+
+type loneVarTestEnv struct {
+	YRURunning string `env:"GIT_GUT"`
 }
